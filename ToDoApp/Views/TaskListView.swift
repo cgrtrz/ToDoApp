023b006e594283388d46d7e44ad22dba1ff1c41d
cgrtrz <150048476+cgrtrz@users.noreply.github.com>
2024-12-06 15:28:32 +0300

    Project setup

---
+++ ToDoApp/Views/TaskListView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskListView.swift
+//  ToDoApp
+//
+//  Created by Cagri Terzi on 06/12/2024.
+//
+
+import SwiftUI
+
+struct TaskListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskListView()
+}
