e91422da140d66b7437de4023c23648ebf66ac70
cgrtrz <150048476+cgrtrz@users.noreply.github.com>
2025-01-07 19:49:32 +0300

    WatchOS App Integrated

---
+++ ToDoAppWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoAppWatch Watch App
+//
+//  Created by Cagri Terzi on 07/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
